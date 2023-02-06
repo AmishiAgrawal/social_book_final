a372cc4729d2d8e5c6eeca169954d9345141dedc
amishi <amiagrwal2002@gmail.com>
2023-02-06 18:24:41 +0530

    commitall

--- sb/social_book/social_book_user/urls.py
+++ sb/social_book/social_book_user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('home',views.uhome),
     path('register',views.register),
     path('login',views.login),
+    path('logout',views.logout),
 ]
--- sb/social_book/social_book_user/views.py
+++ sb/social_book/social_book_user/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib import auth
 # Create your views here.
 def register(request):
-    return render(request,'register.html')
+    if request.method=='POST':
+        uname=request.POST['uname']
+        fname=request.POST['fname']
+        lname=request.POST['lname']
+        email=request.POST['email']
+        pwd=request.POST['pwd']
+        user=User.objects.create_user(username=uname,password=pwd,email=email,first_name=fname,last_name=lname)
+        user.save()
+        messages.success(request,'sucessfully registered')
+        return render(request,'login.html')
+    else:
+        return render(request,'register.html')
 
 def login(request):
-    return render(request,'login.html')
+    if request.method=='POST':
+        uname=request.POST['uname']
+        pwd=request.POST['pwd']
+        user=auth.authenticate(username=uname,password=pwd)
+        if user is not None:
+            auth.login(request,user)
+            return render(request,'uhome.html')
+        else:
+            messages.info(request,'Invalid userid and password')
+            return render(request,'login.html')
+    else:
+        return render(request,'login.html')
 
-def uhome(request):
-    return render(request,'uhome.html')
+def logout(request):
+    auth.logout(request)
+    messages.info(request,'sucessfully logout')
+    return render(request,'login.html')
